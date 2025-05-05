0273962767f52b361581210938b4a212c819b46b
root <root@ip-172-31-42-222.ap-south-1.compute.internal>
2025-05-05 11:31:27 +0000

    first commit

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,69 +1,50 @@
-const express = require('express')
-const router = express.Router()
-const schemas = require('../models/schemas')
+const express = require('express');
+const router = express.Router();
+const schemas = require('../models/schemas');
 
-router.post('/contact/:a', async(req, res) => {
-  // const email = req.body.email
-  // const website = req.body.website
-  const {email, website, message} = req.body
-  const action = req.params.a
-  
-  switch(action) {
-    case "send":
-      const contactData = {email: email, website: website, message: message}
-      const newContact = new schemas.Contact(contactData)
-      const saveContact = await newContact.save()
-      if (saveContact) {
-        res.send('Message sent. Thank you.')
-      } else {
-        res.send('Failed to send message.')
-      }
-      break;
+// POST route for sending contact form
+router.post('/contact/send', async (req, res) => {
+  const { email, website, message } = req.body;
 
-      default:
-        res.send('Invalid Request')
-        break
+  // Validate all required fields
+  if (!email) {
+    return res.status(400).send('Email is required');
+  }
+  if (!website) {
+    return res.status(400).send('Website is required');
+  }
+  if (!message) {
+    return res.status(400).send('Message is required');
   }
 
-  res.end()
-})
+  const contactData = { email, website, message };
 
-router.get('/users', async (req, res) => {
+  // Create a new contact entry in the database
+  const newContact = new schemas.Contact(contactData);
 
-  // uncomment to pull data from the users table using MongoDB
-  // const users = schemas.Users
-  // const userData = await users.find({}).exec()
-  // if (userData) {
-  //   res.send(JSON.stringify(userData))
-  // }
-
-  // this data is being used in the Contact form for the dropdown menu
-  // comment this out and uncomment the mongodb above if you wish to
-  // pull data from the database. However, make sure that this data exists
-  // inside of the contact_form table for it to work
-  const userData = 
-  [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "email": "Sincere@april.biz",
-      "website": "hildegard.org"
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "email": "Shanna@melissa.tv",
-      "website": "anastasia.net"
-    },
-    {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "email": "Nathan@yesenia.net",
-      "website": "ramiro.info"
+  try {
+    const savedContact = await newContact.save();
+    if (savedContact) {
+      return res.send('Message sent. Thank you.');
+    } else {
+      return res.send('Failed to send message.');
     }
-  ]
-  
-  res.send(userData)
-})
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Error saving contact information.');
+  }
+});
 
-module.exports = router
+// GET route for users (example data)
+router.get('/users', async (req, res) => {
+  const userData = [
+    { "id": 1, "name": "Leanne Graham", "email": "Sincere@april.biz", "website": "hildegard.org" },
+    { "id": 2, "name": "Ervin Howell", "email": "Shanna@melissa.tv", "website": "anastasia.net" },
+    { "id": 3, "name": "Clementine Bauch", "email": "Nathan@yesenia.net", "website": "ramiro.info" }
+  ];
+
+  res.send(userData);
+});
+
+module.exports = router;
+
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -8,54 +8,40 @@ function Contact() {
     const [selectData, setSelectData] = useState([])
     const [selectValue, setSelectValue] = useState('')
 
-    useEffect( () => {
+    useEffect(() => {
         let processing = true
         axiosFetchData(processing)
         return () => {
             processing = false
         }
-    },[])
+    }, [])
 
-    // this code shows how to use the React fetch http for GET and POST
-    // const fetchData = async(processing) => {
-    //     const options = {
-    //         method: 'POST',
-    //         headers: {'Content-type': 'application/json'},
-    //         body: JSON.stringify({
-    //             email: email,
-    //             message: message
-    //         })
-    //     }
-    //
-    //     await fetch('https://jsonplaceholder.typicode.com/users')
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         if (processing) {
-    //             setSelectData(data)
-    //         }
-    //     })
-    //     .catch(err => console.log(err))
-    // }
-
-    const axiosFetchData = async(processing) => {
-        await axios.get('http://localhost:4000/users')
-        .then(res => {
-            if (processing) {
-                setSelectData(res.data)
-            }
-        })
-        .catch(err => console.log(err))
+    const axiosFetchData = async (processing) => {
+        await axios.get('http://65.0.72.223:4000/users')
+            .then(res => {
+                if (processing) {
+                    setSelectData(res.data)
+                }
+            })
+            .catch(err => console.log(err))
     }
 
-    const axiosPostData = async() => {
+    const axiosPostData = async () => {
         const postData = {
             email: email,
             website: selectValue,
             message: message
         }
 
-        await axios.post('http://localhost:4000/contact/send', postData)
-        .then(res => setError(<p className="success">{res.data}</p>))
+        await axios.post('http://65.0.72.223:4000/contact/send', postData)
+            .then(res => setError(<p className="success">{res.data}</p>))
+            .catch(err => {
+                if (err.response) {
+                    setError(<p className="required">{err.response.data}</p>)
+                } else {
+                    setError(<p className="required">Something went wrong.</p>)
+                }
+            })
     }
 
     const SelectDropdown = () => {
@@ -63,7 +49,7 @@ function Contact() {
             <select value={selectValue} onChange={(e) => setSelectValue(e.target.value)}>
                 <option value="" key="none"> -- Select One -- </option>
                 {
-                    selectData?.map( (item) => (
+                    selectData?.map((item) => (
                         <option value={item.website} key={item.website}>{item.website}</option>
                     ))
                 }
@@ -74,7 +60,11 @@ function Contact() {
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        if (!message) {
+        if (!email) {
+            setError(<p className="required">Email is required.</p>)
+        } else if (!selectValue) {
+            setError(<p className="required">Please select how you heard about us.</p>)
+        } else if (!message) {
             setError(<p className="required">Message is empty. Please type a message.</p>)
         } else {
             setError('')
@@ -88,14 +78,25 @@ function Contact() {
 
             <form className="contactForm">
                 <label>Email</label>
-                <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
 
                 <label>How Did You Hear About Us?</label>
                 <SelectDropdown />
 
                 <label>Message</label>
-                <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                
+                <textarea
+                    id="message"
+                    name="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+
                 {error}
 
                 <button type="submit" onClick={handleSubmit}>Submit</button>
